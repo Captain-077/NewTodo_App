d1d9d51c3f8ad07723c17fa264293fe29f3c19f3
Your Name <nakuldesigner77@gmail.com>
2024-07-05 23:16:08 +0530

    design updated,icons added check and delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { MdCheck,MdDeleteForever } from "react-icons/md";
+
 
 export default function Todo() {
 
@@ -13,13 +15,13 @@ export default function Todo() {
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
-        if (task.includes(inputValue)){
+        if (task.includes(inputValue)) {
             setInputValue("");
             return;
         }
 
         if (inputValue !== "") {
-            setTask((prev) => [...prev,inputValue]);
+            setTask((prev) => [...prev, inputValue]);
         }
 
         setInputValue("");
@@ -36,6 +38,22 @@ export default function Todo() {
                     <div><input className='todo-input' autoComplete='off' type="text" value={inputValue} onChange={(event) => { handlechange(event.target.value) }} /></div>
                     <button type='submit' className='todo-btn'> Add task</button>
                 </form>
+            </section>
+
+            <section className='mylist'>
+
+                {
+
+                    task.map((item, index) => {
+
+                        return <li key={index} className='todo-item'>
+                            <span>{item}</span>
+                            <button className='checkbtn'><MdCheck /></button>
+                            <button className='deletebtn'><MdDeleteForever/></button>
+                        </li>
+                    })
+                }
+
             </section>
         </section>
     )
